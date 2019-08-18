b801fda93fd56d6b07d57a5db0084c42f282591e
TimDriessen <45215498+TimDriessen@users.noreply.github.com>
2019-08-18 15:50:06 +0200

    Temperature Sensor
    Temperature sensor is working with a mismatch of +- 2 degrees celcius between 20 and 60 degrees celcius. Below 20 and above 60 degrees this mismatch becomes larger.

--- include/Main.h
+++ include/Main.h
@@ -1,3 +1,26 @@
 #pragma once
 
 #include <Arduino.h> 
+
+// ADC
+#define ADC_PIN A0
+#define MAX_ADC 1023.0
+
+// -> Temperature
+#define ADC_CALIBRATE 30
+#define BETA 3660.38 // K (Kelvin)
+#define RESIST_ROOM_TEMP 10000.0 // Ohms
+#define BALANCE_RESISTOR 56000.0 // Ohms 
+const float rgADCtoTemp[]={100.3, 75.1, 52.7, 40.4, 32.1, 25.7, 20.4, 16.0, 12.1, 8.7, 5.5, 2.6}; // lookup table for adc => temperature
+
+// Variables ==================
+
+float fTemperature;   
+
+// ============================
+
+// Functions ==================
+
+float toTemp(int raw); 
+
+// ============================
--- src/Main.cpp
+++ src/Main.cpp
@@ -5,5 +5,34 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("This is a test");
+  int analog = analogRead(ADC_PIN);
+  //voltage = float(analog) / MAX_ADC;
+  fTemperature = toTemp(analog);
+}
+
+// t o T e m p ( )
+// ============================
+// Conversion from ADC value to temperature (celcius)
+// Uses linear interpolation with a lookup table
+//
+float toTemp(int raw) {
+  int raw_calibrate = raw + ADC_CALIBRATE; // Correct for formula / NTC-resistor mismatch
+  int first_index = raw_calibrate / 100; // index of first array element
+  
+  float first_temp = rgADCtoTemp[first_index];
+  float last_temp = rgADCtoTemp[first_index+1];
+
+  int first_adc;
+  if (first_index == 0) { // index == 0 is only index with adc value not being a multiple of 100
+    first_adc = 50; 
+  } else {
+    first_adc = first_index * 100;
+  }
+  int last_adc = (first_index + 1) *  100;
+
+  float temp = first_temp + (raw_calibrate - first_adc) * (last_temp - first_temp) / (last_adc - first_adc); // interpolate
+  
+  //Serial.printf("TEMP: %f, ADC: %i ---- FIRST: adc: %i, temp: %f ---- LAST: adc: %i, temp: %f\n", temp, raw_calibrate, first_adc, first_temp, last_adc, last_temp);
+
+  return temp;
 }
