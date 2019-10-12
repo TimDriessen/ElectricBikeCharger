3f2b8d51dc8dd67bde1f6afc908028cc757440cb
TimDriessen <45215498+TimDriessen@users.noreply.github.com>
2019-10-12 15:07:41 +0200

    Time-dependent State-Machine
    State Machine introduction. It works with one 'state-ticker' and based on this ticker the other ticker methods are called.
    (E.g. state-ticker runs every 0.1 seconds and every 3 seconds we want to measure temperature => after 30 state-ticker runs we execute the measure temperature function.)

--- include/Main.h
+++ include/Main.h
@@ -4,30 +4,72 @@
 #include <Ticker.h>
 #include "AnalogInput.h"
 
-#define MEASURE_UPDATE 2                    // Seconds
-#define CHARGER_STOP 10                     // Hours (With minimum of 1 hour)
+#define SWITCH_PIN //TODO: Choose pin       // Pin used for switch
+#define LED_PIN 4                           // Pin used for state-indicating led
+
+#define NUM_CELLS 20                        // Number of cells inside battery pack
+
+#define STATE_TICK 0.1                      // Tick-time for state-machine (seconds)
+
+#define CHECK_START 3//0.5                  // Tick-time: Checking start (seconds)
+#define CHECK_END 3//2                      // Tick-time: Checking stop (seconds)
+#define TIMEOUT 10                          // Tick-time: Timeout (hours)
+#define LED_SWITCH 0.2                      // Tick-time: Led switching (seconds)
+#define CHECK_RESET 3//0.5                  // Tick-time: Checking reset (seconds)
+
+#define DT_T1_END 0.6                       // Max dT1/dt
+#define DT_V2_END -0.005*NUM_CELLS          // Min dV2
+#define V2_END 1.6*NUM_CELLS                // Max V2            
+#define T1_END 50                           // Max T1
+#define T2_END 100                          // Max T2
+
+#define VZERO_TRESHOLD 0.2                  // <= VZERO_TRESHOLD, implies no voltage
+#define VBATT_TRESHOLD 18                   // >= VBATT_TRESHOLD, implies battery connected
+
+#define DELTA_V2_DUR 10                     // Amount of seconds 'dV2' should last, to trigger stop
+#define DELTA_T1_DUR 10                     // Amount of seconds 'dT1/dt should last, to trigger stop
 
 // Variables ==================
 
-int iStopCounter = CHARGER_STOP;            // We count every hour since Ticker doesn't go further than +- 1.9 hours
-
-bool bChargerConnected = false;
+int iGlobalState = 0;                       // 0: Waiting (Led off) - 1: Charging (Led blinking) - 2: Charge stopped (Led on)
+bool bLedState = false;             
 
 float fTemperatureT1;                       // Battery temperature
 float fTemperatureT2;                       // Heatsink temperature
 float fVoltageV1;                           // Charger voltage 
 float fVoltageV2;                           // Battery voltage
 
-Ticker tChargerStop;
-Ticker tMeasure;
+float fdT1dt, fdV2;                         // Deltas from temperature / voltage
+float fV2dur, fT1dur = 0;                   // Duration of meeting condition
+
+float fTimeState1 = 0;                      // Time-state variables
+float fTimeState2_end, 
+fTimeState2_timeout, fTimeState2_led = 0;
+float fTimeState3 = 0;
+
+Ticker tStateMachine;                       // Ticker for State Machine
 CAnalogInput aiAnalog;                      // Calls default (empty) constructor
 
 // ============================
 
 // Functions ==================
 
+void timeUpdate(float &time_var, float limit, void (*call)());
+void resetTimers();
+void stateMachineRun();
+
 void measureAnalog();
-void checkCharger();
+
+void checkStart();
+void checkStop();
+void checkReset();
+
+void startCharger();
 void stopCharger();
+void resetCharger();
+void timeOut();
+
+void setLed(bool led_state);
+void ledSwap();
 
 // ============================
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,55 +2,169 @@
 
 void setup() {
   Serial.begin(115200);
-} 
+
+  pinMode(LED_PIN, OUTPUT); 
+  digitalWrite(LED_PIN, LOW);
+
+  tStateMachine.attach(STATE_TICK, stateMachineRun); // Start state-machine ticker
+  resetCharger(); // Setup initial state
+}  
 
 void loop() {
-  measureAnalog();
+  
+}
+
+// t i m e U p d a t e ()
+// ============================
+// Updates 'time_var' and if then 'limit' is exceed it calls the function 'call'
+//
+void timeUpdate(float &time_var, float limit, void (*call)()) {
+  time_var += STATE_TICK;
+  if (time_var >= limit) {
+    time_var = 0;
+    (*call)();
+  }
+}
+
+// r e s e t T i m e r s ()
+// ============================
+// Reset all state-timers from the state-machine
+//
+void resetTimers() { 
+  fTimeState1 = fTimeState2_led = fTimeState2_end = fTimeState2_timeout = fTimeState3 = 0; 
+}
+
+// t i m e U p d a t e ()
+// ============================
+// Updates 'time_var' and if then 'limit' is exceed it calls the function 'call'
+//
+void stateMachineRun() {
+  switch(iGlobalState) {
+    case 0:
+      timeUpdate(fTimeState1, CHECK_START, checkStart);
+      break;
+    case 1:
+      timeUpdate(fTimeState2_led, LED_SWITCH, ledSwap); // Note to myself: If you place this line below the CHECK_END line, led could fail => checkStop turns it on, but ledSwap can still turn it off
+      timeUpdate(fTimeState2_end, CHECK_END, checkStop);
+      //timeUpdate(fTimeState2_timeout, (TIMEOUT * 3600), timeOut);
+      break;
+    case 2:
+      timeUpdate(fTimeState3, CHECK_RESET, checkReset);
+      break;
+  }
 }
 
 // m e a s u r e A n a l o g ()
 // ============================
-// Measure temperatures and voltages
+// Measure temperatures and voltages and calculate dT1/dt and dV2
 //
 void measureAnalog() {
-  fTemperatureT1 = aiAnalog.measureTemp(true);
+  float t1 = aiAnalog.measureTemp(true);
+  fdT1dt = ((t1 - fTemperatureT1) / CHECK_END) * 60;
+  fTemperatureT1 = t1;
+
   fTemperatureT2 = aiAnalog.measureTemp(false);
-  fVoltageV1 = aiAnalog.measureVolt(true); 
-  fVoltageV2 = aiAnalog.measureVolt(false); 
+  fVoltageV1 = aiAnalog.measureVolt(true);
+
+  float v2 = aiAnalog.measureVolt(false); 
+  fdV2 = v2 - fVoltageV2;
+  fVoltageV2 = v2; // TODO: CHARGER SHOULD BE TURNED OFF TO BE ABLE TO MEASURE BATTERY VOLTAGE!!! (Or should it?)
 }
 
-
-// c h e c k C h a r g e r ()
+// s t a r t C h a r g e r ()
 // ============================
-// Checks state of charger (connected / disconnected)
+// Start charging process (State 0 -> 1)
 //
-void checkCharger() {
-  //TO-DO: Check charger
+void startCharger() {
+  iGlobalState = 1;
+  resetTimers();
+  Serial.printf("state: %i \n", iGlobalState);
 
-  // If charger is connected
-  iStopCounter = CHARGER_STOP; // reset counter
-  tChargerStop.attach(3600, stopCharger); // 1 hour
-  tMeasure.attach(2, measureAnalog); // Do analog measurements
-
-  // If charger is disconnected
-  tChargerStop.detach();
-  tMeasure.detach();
+  // TODO: Start fan
 }
 
 // s t o p C h a r g e r ()
 // ============================
-// Called by Ticker to stop charging process
+// Stop charging process (State 1 -> 2)
 //
 void stopCharger() {
-  iStopCounter--;
+  iGlobalState = 2;
+  resetTimers();
+  setLed(true); // Turn led on
+  Serial.printf("state: %i \n", iGlobalState);
+}
 
-  //Serial.printf("Hours left: %i\n", iStopCounter);
+// r e s e t C h a r g e r ()
+// ============================
+// Reset charger (State 2 -> 0)
+//
+void resetCharger() {
+  iGlobalState = 0;
+  resetTimers();
+  setLed(false); // Turn led off
+  Serial.printf("state: %i \n", iGlobalState);
 
-  if (iStopCounter == 0) {
-    tChargerStop.detach();
-    tMeasure.detach();
-    
-    //Serial.println("Stopping Charger");
-    //TO-DO: Stop charging process
-  }
+  // TODO: Stop fan
+}
+
+// t i m e O u t ()
+// ============================
+// Called if charger-time is finished
+//
+void timeOut() {
+  Serial.println("Stopping Charger - Reason: Timer");
+  stopCharger();
+}
+
+
+// c h e c k S t a r t ()
+// ============================
+// Check charger for starting conditions (battery connected && switch on)
+//
+void checkStart() {
+  startCharger(); // temporary
+
+  // TODO: Check toggle switch
+  // TODO: Check battery connected
+  
+  // If battery and switch => startCharger();
+}
+
+// c h e c k S t o p ()
+// ============================
+// Check charger for stopping conditions (temperatures / voltages)
+//
+void checkStop() {
+  stopCharger(); // temporary
+  
+  // TODO: Measure temperature and voltage and calculate all dT/dt and dV values
+  // TODO: Use these values to check for stopping conditions
+}
+
+// c h e c k R e s e t ()
+// ============================
+// Check charger for reset conditions (battery disconnect || switch)
+//
+void checkReset() {
+  resetCharger(); // temporary
+
+  // TODO: Check toggle switch
+  // TODO: Check battery disconnected
+}
+
+// l e d S w a p
+// ============================
+// Swap led state from on to off and vice versa
+//
+void ledSwap() { 
+  setLed(!bLedState); 
+}
+
+// s e t L e d ()
+// ============================
+// Set led to 'led_state'
+//
+void setLed(bool led_state) {
+  bLedState = led_state;
+  digitalWrite(LED_PIN, led_state);
 }
